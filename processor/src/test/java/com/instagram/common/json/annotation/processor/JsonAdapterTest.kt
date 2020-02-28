b833c772409eae6255d56fbe3cfa5e31b6467cd1
Kjartan Nordbo <knordbo@fb.com>
2020-02-28 10:21:03 -0800

    Add JsonAdapter - Add JsonAdapter test
    Summary:
    # This stack
    Adds `JsonAdapter`, `FromJson` and `ToJson` annotations to serialize and deserialize types. Currently enabled for enum types whenever no custom custom `valueExtractFormatter`, `serializeCodeFormatter` etc. exists.
    
    This feature is intended to not mess with any existing logic, which is why it bails out when it seems custom formatters. So custom formatters take priority over the adapter, but this covers most of the custom formatters which seems to be used for enums.
    
    This means code goes from (see part 5 of stack for example):
    
    ```
    JsonField(
          fieldName = "review_status",
          valueExtractFormatter =
              "ProductReviewStatus.fromServerValue(${parser_object}.getValueAsString())",
          serializeCodeFormatter =
              "${generator_object}.writeStringField(\"${json_fieldname}\", "
                  + "${object_varname}.${field_varname}.serverValue)")
    ProductReviewStatus mReviewStatus = ProductReviewStatus.APPROVED;
    ```
    
    to
    
    ```
    JsonField(fieldName = "review_status")
    ProductReviewStatus mReviewStatus = ProductReviewStatus.APPROVED;
    
    JsonAdapter(adapterClass = ProductReviewStatus.class)
    public enum ProductReviewStatus {
    
      ...
    
      FromJson
      public static ProductReviewStatus fromServerValue(String serverValue) {
        return ProductReviewStatus.valueOf(serverValue);
      }
    
      ToJson
      public static String toServerValue(ProductReviewStatus productReviewStatus) {
        return productReviewStatus.serverValue;
      }
    }
    
    ```
    # This diff
    
    Adds tests
    
    Reviewed By: mgoovaer
    
    Differential Revision: D20044218
    
    fbshipit-source-id: 92b5e1222894912b5aff0be2ff46858faae25fd4

---
+++ processor/src/test/java/com/instagram/common/json/annotation/processor/JsonAdapterTest.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.instagram.common.json.annotation.processor
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Testing whether the [JsonAnnotationProcessor] generates valid Java code when
+ * using a [JsonAdapter].
+ */
+class JsonAdapterTest {
+
+  private val myEnumHolder = MyEnumHolder(myEnum = MyEnum.BAR)
+
+  private val myEnumHolderJson = """
+    {"my_enum":"bar"}
+    """.trimIndent()
+
+  @Test
+  fun serializeEnumWithJsonAdapter() {
+    val json = MyEnumHolder__JsonHelper.serializeToJson(myEnumHolder)
+
+    Assert.assertEquals(myEnumHolderJson, json)
+  }
+
+  @Test
+  fun parseEnumWithJsonAdapter() {
+    val parsedMyEnumHolder = MyEnumHolder__JsonHelper.parseFromJson(myEnumHolderJson)
+
+    Assert.assertEquals(myEnumHolder, parsedMyEnumHolder)
+  }
+}
---
+++ processor/testuut/kotlin/src/main/java/com/instagram/common/json/annotation/processor/EnumJsonAdapter.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.instagram.common.json.annotation.processor
+
+import com.instagram.common.json.annotation.FromJson
+import com.instagram.common.json.annotation.JsonAdapter
+import com.instagram.common.json.annotation.JsonField
+import com.instagram.common.json.annotation.JsonType
+import com.instagram.common.json.annotation.ToJson
+
+@JsonType
+data class MyEnumHolder(
+    @JsonField(fieldName = "my_enum")
+    var myEnum: MyEnum = MyEnum.NONE
+)
+
+@JsonAdapter(adapterClass = MyEnumAdapter::class)
+enum class MyEnum(val serverValue: String) {
+  FOO("foo"),
+  BAR("bar"),
+  NONE("")
+}
+
+object MyEnumAdapter {
+
+  private val reverseMap = MyEnum.values().associateBy(MyEnum::serverValue)
+
+  @JvmStatic
+  @FromJson
+  fun fromJson(serverValue: String?): MyEnum = reverseMap[serverValue] ?: MyEnum.NONE
+
+  @JvmStatic
+  @ToJson
+  fun toJson(value: MyEnum): String? = value.serverValue
+}
